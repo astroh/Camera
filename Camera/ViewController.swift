bcb596ef2dc82af79e6c004ed31cef3b6cf677f2
astroh <izzyrael@me.com>
2016-02-11 19:59:48 -0500

    First Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Izzy Rael on 2/9/16.
+//  Copyright © 2016 Izzy Rael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var photoLibrary: UIButton!
+    @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func photoLibraryAction(sender: UIButton) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraButtonAction(sender: UIButton) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+
+
+
